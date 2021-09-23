dee6be7f93c2ade7bbe89a6a765acdb1c85254fa
CraxisDev <franciscopdejesus@hotmail.com>
2021-09-23 15:41:43 +0200

    Actions and Forms displayed

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 
 
 class Course(models.Model):
-    _name = 'openacademy.course'
+    _name = 'course'
     _description = 'Course'
     
     name = fields.Char('Name', required=True)
