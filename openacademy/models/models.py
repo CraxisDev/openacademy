772979fcd41bd4b079051d6037d72c536e12390d
CraxisDev <franciscopdejesus@hotmail.com>
2021-09-27 11:06:02 +0200

    Model and view of session created

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -9,3 +9,12 @@ class Course(models.Model):
     
     name = fields.Char('Name', required=True)
     description = fields.Text('Description', required=False)
+    
+class Session(models.Model):
+    _name = 'session'
+    _description = 'Session'
+    
+    name = fields.Char('Name', required=True)
+    start_date = fields.Char('Date Start', required=True)
+    duration = fields.Float('Duration', help="Duration in days", required=True)
+    seats = fields.Integer('Seats', help="Seats Quantity", required=False)
