d35e9dc25699b6149f4d437a473ddbb48baae04c
CraxisDev <franciscopdejesus@hotmail.com>
2021-09-27 14:32:12 +0200

    [ADD] Adden instructor field to session and responsible field to course

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -9,12 +9,14 @@ class Course(models.Model):
     
     name = fields.Char('Name', required=True)
     description = fields.Text('Description', required=False)
+    responsible = fields.Many2one('res.users')
     
 class Session(models.Model):
     _name = 'session'
     _description = 'Session'
     
     name = fields.Char('Name', required=True)
+    instructor = fields.Many2one('res.partner')
     start_date = fields.Date('Date Start', required=True)
     duration = fields.Float('Duration', help="Duration in days", required=True)
     seats = fields.Integer('Seats', help="Seats Quantity", required=False)
